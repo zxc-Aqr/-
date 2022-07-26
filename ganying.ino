4ca9b8e6889bf425ace7988f66cae8a986e1eaac
zxc-Aqr <94050861+zxc-Aqr@users.noreply.github.com>
2022-07-26 19:40:00 +0800

    Add files via upload

---
+++ ganying.ino
@@ -0,0 +1,40 @@
+#include<Servo.h>
+#define DuoPIN D5
+#define Echo D2
+#define Trig D8
+
+Servo myDuoJi;
+long getTime()
+{
+     digitalWrite(Trig, HIGH);
+     delayMicroseconds(10);
+     digitalWrite(Trig,LOW);
+     
+     return pulseIn(Echo,HIGH);
+}
+
+void initCSB()
+{
+   pinMode(Echo, INPUT);
+   pinMode(Trig, OUTPUT);
+}
+
+void setup(){
+    initCSB();
+   myDuoJi.attach(DuoPIN);
+    Serial.begin(115200);
+}
+
+void loop(){
+    long dis;
+    dis = getTime()/58;
+
+  
+    if(dis <10){
+         myDuoJi.write(45);
+      delay(2000);
+   }else{
+        myDuoJi.write(270);
+     }
+  
+}
